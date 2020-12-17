f3b4e21a05923ac974fe7e8e57823f939f4c01de
mhubii <m.huber_1994@hotmail.de>
2020-12-17 14:49:10 +0000

    updated decklink capture

--- cmake/FindDeckLinkAPI.cmake
+++ cmake/FindDeckLinkAPI.cmake
@@ -1,17 +1,17 @@
 find_library(
-    DeckLinkAPI_LIBRARY
+    DeckLinkAPI_LIBRARIES
     NAMES DeckLinkAPI
     PATHS /usr/lib /usr/local/lib
     REQUIRED 
 )
 
 find_path(
-    DeckLinkAPI_INCLUDE_DIR
+    DeckLinkAPI_INCLUDE_DIRS
     NAMES DeckLinkAPI.h
     PATHS ${DECKLINK_SDK_DIR}/include
     REQUIRED
 )
 
-if (NOT (DeckLinkAPI_LIBRARY AND DeckLinkAPI_INCLUDE_DIR))
+if (NOT (DeckLinkAPI_LIBRARIES AND DeckLinkAPI_INCLUDE_DIRS))
     message(FATAL_ERROR "Couldn't find DeckLinkAPI, make sure to set DECKLINK_SDK_DIR.")
-endif(NOT (DeckLinkAPI_LIBRARY AND DeckLinkAPI_INCLUDE_DIR))
+endif(NOT (DeckLinkAPI_LIBRARIES AND DeckLinkAPI_INCLUDE_DIRS))
---
+++ src/decklink_capture.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <functional>
+#include <DeckLinkAPI.h>
+
+
+class DeckLinkCapture : public IDeckLinkInputCallback {
+    public:
+        typedef std::function<void (long unsigned frame_count, void* raw_frame, unsigned raw_frame_width, unsigned raw_frame_height, unsigned bytes_per_pixel)> DeckLinkCaptureCallbackType;
+
+        DeckLinkCapture();
+
+        virtual HRESULT STDMETHODCALLTYPE QueryInterface(REFIID iid, LPVOID *ppv) { return E_NOINTERFACE; }
+        virtual ULONG STDMETHODCALLTYPE AddRef(void);
+        virtual ULONG STDMETHODCALLTYPE  Release(void);
+        virtual HRESULT STDMETHODCALLTYPE VideoInputFormatChanged(BMDVideoInputFormatChangedEvents, IDeckLinkDisplayMode*, BMDDetectedVideoInputFormatFlags) { return S_OK; };
+        virtual HRESULT STDMETHODCALLTYPE VideoInputFrameArrived(IDeckLinkVideoInputFrame*, IDeckLinkAudioInputPacket*);
+
+        bool init();
+        void setCallback(DeckLinkCaptureCallbackType callback) { callback_ = callback; };
+
+    private:
+        int32_t	refCount_;
+        unsigned long frame_count_;
+
+        DeckLinkCaptureCallbackType callback_;
+};
