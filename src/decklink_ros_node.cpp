06305e643debdf1e85f571621dac1e3a953ea9e4
mhubii <m.huber_1994@hotmail.de>
2020-12-17 17:25:29 +0000

    updated node

--- src/decklink_ros_node.cpp
+++ src/decklink_ros_node.cpp
@@ -1,18 +1,18 @@
 #include <ros/ros.h>
 
-#include <decklink_ros/decklink_capture.h>
+#include <decklink_ros/ros_image_pub_thread.h>
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "decklink_ros_node");
     ros::NodeHandle nh;
 
     int device_id, mode_id, pixel_format_id;
-    
-    nh.getParam("decklink/device_id", device_id);
-    nh.getParam("decklink/mode_id", mode_id);
-    nh.getParam("decklink/pixelformat_id", pixel_format_id);
 
-    DeckLinkCapture capture();
+    RosImagePubThread img_pub_thread(nh);
+    if (!img_pub_thread.init()) {
+        ROS_ERROR("decklink_ros_node: Failed to initialize RosImagePubThread.");
+        std::exit(-1);
+    }
 
     ros::spin();
 
