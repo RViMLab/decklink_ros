71f4de29f749866cf0651988af9c08892828c45c
mhubii <m.huber_1994@hotmail.de>
2022-03-26 20:40:06 +0000

    added a launch file

---
+++ launch/decklink_ros_node.launch.py
@@ -0,0 +1,38 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    launch_args = []
+    launch_args.append(
+        DeclareLaunchArgument(
+            name="cname",
+            default_value="decklink",
+            description="Camera name."
+        )
+    )
+
+    launch_args.append(
+        DeclareLaunchArgument(
+            name="url",
+            default_value="''",
+            description="Camera calibration YAML file."
+        )
+    )
+
+    decklink_camera = Node(
+        package="decklink_ros",
+        executable="decklink_ros_node",
+        parameters=[
+            {"cname": LaunchConfiguration("cname")},
+            {"url": LaunchConfiguration("url")}
+        ]
+    )
+
+    return LaunchDescription(
+        launch_args + [
+            decklink_camera
+    ])
